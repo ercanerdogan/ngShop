6bcfb85778bdfe9deb23516bc1d8f0ef741d2a0c
ercanerdogan@gmail.com <ercanerdogan@gmail.com>
2021-04-14 01:34:47 +0300

    "cross origin resource sharing support was added"

--- API/Startup.cs
+++ API/Startup.cs
@@ -28,7 +28,13 @@ namespace API
 
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
-
+            services.AddCors(opt=>
+            {
+                opt.AddPolicy("CorsPolicy", policy=>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
           
 
         }
@@ -52,7 +58,10 @@ namespace API
             app.UseRouting();
             app.UseStaticFiles();
 
+            app.UseCors("CorsPolicy");
+
             app.UseAuthorization();
+            
 
             app.UseEndpoints(endpoints =>
             {
