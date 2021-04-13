3370f64777fda377b34ee872ae58e2a826887f6f
ercanerdogan@gmail.com <ercanerdogan@gmail.com>
2021-04-14 01:27:49 +0300

    search products by name functionality has been added

--- Core/Specifications/ProductSpecParams.cs
+++ Core/Specifications/ProductSpecParams.cs
@@ -17,5 +17,12 @@ namespace Core.Specifications
 
         public string Sort { get; set; }
 
+        private string _search;
+        public string Search 
+        { 
+            get => _search;
+            set => _search = value.ToLower();
+        }
+
     }
 }
--- Core/Specifications/ProductWithFiltersCountSpecification.cs
+++ Core/Specifications/ProductWithFiltersCountSpecification.cs
@@ -6,6 +6,7 @@ namespace Core.Specifications
     {
         public ProductWithFiltersCountSpecification(ProductSpecParams productParams)
             :  base(x=>
+                (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains(productParams.Search)) && 
                 (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) && 
                 (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
             )
