66ee5ab82ae8a90d592692d4400cd10fdee9f936
ercanerdogan@gmail.com <ercanerdogan@gmail.com>
2021-04-14 00:17:24 +0300

    add filtering in specificationPattern

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -34,9 +34,9 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts(string sort)
+        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts(string sort, int? brandId, int? typeId)
         {
-            var spec = new ProductsWithTypesAndBrandsSpecification(sort);
+            var spec = new ProductsWithTypesAndBrandsSpecification(sort, brandId, typeId);
 
             var products = await _productRepo.ListAsync(spec);
 
